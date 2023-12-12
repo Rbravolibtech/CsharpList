b35d95c26c3359533daa43a3d7809c03f7475469
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-12 12:25:05 -0700

    coded c# lists

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CsharpList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new List<int>() { 1, 2, 3, 4 };
+        numbers.Add(1);
+        numbers.AddRange(new int[3] { 5, 6, 7 });
+
+        foreach (var number in numbers)
+            Console.WriteLine(number);
+
+        Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
+        Console.WriteLine("Index of 1: " + numbers.LastIndexOf(1));
+
+        Console.WriteLine("count: " + numbers.Count);
+
+        for (var i = 0; i < numbers.Count; i++)
+        foreach (var number in numbers)
+        {
+            if (number == 1)
+                    numbers.Remove(numbers[i]);
+
+        }
+        foreach (var number in numbers)
+            Console.WriteLine(number);
+
+        numbers.Clear();
+        Console.WriteLine("Count: " + numbers.Count);
+    
+    }
+}
+
